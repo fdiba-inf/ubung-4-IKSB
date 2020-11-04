984f54bcf31df720a59ed295180a017723b6db39
IKSB <iikostov2001@gmail.com>
2020-11-04 08:27:47 +0000

    ...

--- src/exercise4/ArrayInputOutput.java
+++ src/exercise4/ArrayInputOutput.java
@@ -1,10 +1,8 @@
 package exercise4;
-
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayInputOutput {
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
